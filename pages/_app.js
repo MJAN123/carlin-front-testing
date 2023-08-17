0fdfbc428194bbe4cb2a0e83a402adb872c89b07
Muhammad Jan <m.janbugti12@gmail.com>
2023-08-17 11:44:19 +0500

    chnaged view port

--- pages/_app.js
+++ pages/_app.js
@@ -19,10 +19,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=5, viewport-fit=cover"
-          name="viewport"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta content="#000000" name="theme-color" />
         <meta content="Carlin Solutions" name="description" />
 
