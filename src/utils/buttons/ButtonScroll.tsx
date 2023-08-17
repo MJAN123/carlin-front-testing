02a1b60831540136f6d1744bbdaf5c4a45d5cb7e
Muhammad Jan <m.janbugti12@gmail.com>
2023-08-17 13:08:11 +0500

    hide react-scroll

--- src/utils/buttons/ButtonScroll.tsx
+++ src/utils/buttons/ButtonScroll.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react"
-import { Link as LinkScroll } from "react-scroll"
+// import { Link as LinkScroll } from "react-scroll"
 
-const ButtonScroll = ({ title, id }: { title: string; id: any }) => {
+const ButtonScroll = ({ title }: { title: string; id?: any }) => {
   return (
     <>
-      <LinkScroll
+      <button
         className="rounded-10 bg-gradient-to-r from-blue to-pink py-10 px-24 text-18 text-white"
-        offset={-80}
-        to={id}
+        // offset={-80}
+        // to={id}
       >
         {title}
-      </LinkScroll>
+      </button>
     </>
   )
 }
